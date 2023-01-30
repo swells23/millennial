50e1cf190d749726589ce9c2f1a83fa6b65e7280
Stephen Wells <wells.sn2@gmail.com>
2023-01-29 23:02:51 -0500

    refactor variables for api paths

---
+++ src/data/templateMeta.tsx
@@ -0,0 +1,2 @@
+export const DOMAIN: string = 'https://jonesmillennial.com',
+    WP_API: string = 'wp-json/wp/v2';
--- src/pages/api/Page.tsx
+++ src/pages/api/Page.tsx
@@ -1,5 +1,7 @@
+import { DOMAIN, WP_API } from '../../data/templateMeta';
+
 export default async function Page(slug: string) {
-    const res = await fetch(`https://jonesmillennial.com/wp-json/wp/v2/pages?slug=${slug}`),
+    const res = await fetch(`${DOMAIN}/${WP_API}/pages?slug=${slug}`),
         pageData = await res.json();
 
     return {
