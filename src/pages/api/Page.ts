fef2053b2c1c2db0f606fa1ef2e08af55f328089
Stephen Wells <wells.sn2@gmail.com>
2023-01-30 14:23:30 -0500

    add error handling for Page api

--- src/pages/api/Page.ts
+++ src/pages/api/Page.ts
@@ -1,12 +1,24 @@
 import { DOMAIN, WP_API } from '../../data/templateMeta';
 
-export default async function Page(slug: string) {
-    const res = await fetch(`${DOMAIN}/${WP_API}/pages?slug=${slug}`),
-        pageData = await res.json();
+interface PageData {
+    props: {
+        pageData: JSON
+    }
+}
 
-    return {
-        props: {
-            pageData
+export default async function Page(slug: string): Promise<PageData | undefined> {
+    try {
+        const res: Response = await fetch(`${DOMAIN}/${WP_API}/pages?slug=${slug}`),
+            pageData: JSON = await res.json();
+
+        return {
+            props: {
+                pageData
+            }
         }
+    } catch (err: any) {
+        console.error(`Error: Unable to retreive page data. ${err.name}: ${err.message}`);
+
+        return;
     }
 }
