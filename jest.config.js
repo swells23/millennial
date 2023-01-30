435e142fed4f5d155b08ade1d2a965917a238eff
Stephen Wells <wells.sn2@gmail.com>
2023-01-30 18:08:50 -0500

    properly configure Jest & testing-library for typescript usage

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const nextJest = require('next/jest'),
+  createJestConfig = nextJest({
+    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+    dir: './',
+  });
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig();
