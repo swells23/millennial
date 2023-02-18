de7120cd43d56f610839b28718d7e2c1b2d67003
Stephen Wells <wells.sn2@gmail.com>
2023-02-18 14:05:26 -0500

    Revert "next img fill test"
    This reverts commit 93c916a4bc0e6d5706a6562934ef5f08d863fc4e.

--- src/components/Hero/Hero.styles.ts
+++ src/components/Hero/Hero.styles.ts
@@ -15,10 +15,10 @@ const styles: any = {
     width: "100%",
     "& > img": {
       filter: "brightness(50%)",
-      // height: "inherit",
+      height: "inherit",
       objectFit: "inherit",
       objectPosition: "inherit",
-      // width: "inherit",
+      width: "inherit",
     },
   },
   heroImgSx: {
