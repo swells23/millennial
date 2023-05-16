d7fbcb9b822392ac88afa3a492525ca2a33fafab
Stephen Wells <wells.sn2@gmail.com>
2023-05-15 20:57:07 -0400

    separate seo into own component

--- src/components/layout/DefaultTemplate/DefaultTemplate.tsx
+++ src/components/layout/DefaultTemplate/DefaultTemplate.tsx
@@ -1,7 +1,7 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Head from "next/head";
 import React, { ReactNode } from "react";
-import { GlobalFooter, GlobalNav } from "../../pattern";
+import { GlobalFooter, GlobalNav, Seo } from "../../pattern";
 
 export default function DefaultTemplate({
   children,
@@ -12,28 +12,15 @@ export default function DefaultTemplate({
   title?: string;
   metaDesc?: string;
 }) {
-  const pageTitle = title && `${title} - `;
+  const titleTransform = title && `${title} - `,
+    pageTitle = `${titleTransform}Millennial Realty & Investments LLC`;
 
   return (
     <>
       <Head>
-        <title>{`${pageTitle}Millennial Realty & Investments LLC`}</title>
+        <title>{pageTitle}</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <meta name="description" content={metaDesc} />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content={`${pageTitle}Millennial Realty & Investments LLC`}
-        />
-        <meta property="og:description" content={metaDesc} />
-        <meta
-          property="og:site_name"
-          content={`${pageTitle}Millennial Realty & Investments LLC`}
-        />
-        <meta
-          charSet="keywords"
-          content="Real Estate,Millennial,Realty,Investments,Stanley Jones"
-        />
+        <Seo title={pageTitle} description={metaDesc} />
       </Head>
       <CssBaseline />
       <GlobalNav />
---
+++ src/components/pattern/Seo/Seo.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { DOMAIN } from "../../../data/templateMeta";
+
+export default function Seo({
+    title,
+    description,
+}: {
+    title?: string;
+    description?: string;
+}) {
+    return (
+        <>
+            <meta name="description" content={description} />
+            <meta property="og:site_name" content={title} />
+            <meta property="og:title" content={title} />
+            <meta property="og:url" content={DOMAIN} />
+            <meta property="og:description" content={description} />
+            <meta property="og:type" content="website" />
+        </>
+    );
+}
+
+Seo.defaultProps = {
+    title: "",
+    metaDesc: "",
+};
