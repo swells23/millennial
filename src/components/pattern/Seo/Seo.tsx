5c82549e2ae80d41634deb9488f3782a75cd7dd2
Stephen Wells <wells.sn2@gmail.com>
2023-05-15 23:56:55 -0400

    Revert "use next/image for agent cards"
    This reverts commit fe9b5305bca6877785974fa74c3224b52c83006c.

--- src/components/pattern/Seo/Seo.tsx
+++ src/components/pattern/Seo/Seo.tsx
@@ -2,25 +2,25 @@ import React from "react";
 import { DOMAIN } from "../../../data/templateMeta";
 
 export default function Seo({
-  title,
-  description,
+    title,
+    description,
 }: {
-  title?: string;
-  description?: string;
+    title?: string;
+    description?: string;
 }) {
-  return (
-    <>
-      <meta name="description" content={description} />
-      <meta property="og:site_name" content={title} />
-      <meta property="og:title" content={title} />
-      <meta property="og:url" content={DOMAIN} />
-      <meta property="og:description" content={description} />
-      <meta property="og:type" content="website" />
-    </>
-  );
+    return (
+        <>
+            <meta name="description" content={description} />
+            <meta property="og:site_name" content={title} />
+            <meta property="og:title" content={title} />
+            <meta property="og:url" content={DOMAIN} />
+            <meta property="og:description" content={description} />
+            <meta property="og:type" content="website" />
+        </>
+    );
 }
 
 Seo.defaultProps = {
-  title: "",
-  metaDesc: "",
+    title: "",
+    metaDesc: "",
 };
